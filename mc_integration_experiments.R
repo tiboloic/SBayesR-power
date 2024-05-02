1859927c8a61db46a0f875881bc98f8697c1770a
Loic Thibaut <Loic.Thibaut@gmail.com>
2024-05-03 05:57:32 +1000

    more experiments

--- mc_integration_experiments.R
+++ mc_integration_experiments.R
@@ -12,6 +12,10 @@ bs <- runif(N)
 mean(dchisq(-log(as), 1, -log(bs) * n / (1 - h2)) / as * dnorm(sqrt(-log(bs)), 0, sqrt(h2 / m / pi)) / sqrt(-log(bs)) / bs)
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 9385f8f (more experiments)
 # simpler try
 f <- function(x) {
   dnorm(x[1], 0, 1) + dnorm(x[2], 0, 1)
@@ -25,3 +29,23 @@ g <- function(x) {
 }
 cubintegrate(g, lower=c(0,0), upper=c(1, 1), method = "pcubature")
 
+<<<<<<< HEAD
+=======
+# analytical example
+# a compound random variable:exponential distributed with a rate from a [0,1] uniform
+
+# analytical solution
+exp_unif_anal <- function(x) {
+  1 + exp(-x)/x -1/x
+}
+
+# MC integration by compound drawing
+mean(rexp(10000, runif(10000))<2)
+
+# MC integration by drawing from mixing distribution
+
+# MC integration by double integral
+as= runif(1000)
+bs = runif(1000)
+mean(as * exp(-as * bs))
+>>>>>>> 9385f8f (more experiments)
