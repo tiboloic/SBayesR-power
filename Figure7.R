adb24cea364a455eaa7a53b155fec20e51f74b4e
tiboloic <Loic.Thibaut@my.jcu.edu.au>
2024-05-30 17:53:00 +1000

    modified plot to shpw number of SNPs instead of power

--- Figure7.R
+++ Figure7.R
@@ -3,19 +3,40 @@
 #  Figure 7:power and proportion of variance
 #  
 
-# height
-h2 <- 0.68
-pis <- c(9262822.000000, 60758.699219, 2834.479980, 190.014999, 0.135000)
-gammas <- c(0, 1e-5, 1e-4, 1e-3, 1e-2)
-
 # sample sizes
 ns <- 10 ^ seq(4, 7, 0.1)
 
-ht_power <- sapply(ns, function(n) pow_R_e(0.9, n = n, h2 = h2,
-                               gammas = c(0, 1e-5, 1e-4, 1e-3, 1e-2),
-                               pis = pis))
-ht_power_mc <- sapply(ns, function(n) pow_R_mc(0.9, n = n, h2 = h2,
-                                           gammas = c(0, 1e-5, 1e-4, 1e-3, 1e-2),
-                                           pis = pis))
+# height
+h2 <- 0.68
+ms <- c(9262822.000000, 60758.699219, 2834.479980, 190.014999, 0.135000)
+m_tot <- sum(ms)
+pis <- ms / m_tot
+gammas <- c(0, 1e-5, 1e-4, 1e-3, 1e-2)
+
+ht_power <- m_tot * sapply(ns, function(n) pow_R_e(0.9, n = n, h2 = h2,
+                               gammas = gammas, pis = pis))
+ht_power_mc <- m_tot * sapply(ns, function(n) pow_R_mc(0.9, n = n, h2 = h2,
+                                           gammas = gammas, pis = pis))
 plot(ht_power ~ ns, log = 'xy', type = "l", col = 2)
 lines(ht_power_mc ~ ns, col = 3)
+
+# SimLD
+h2 <- 0.5
+ms <- c(1028069, 0, 10145, 4, 0)
+m_tot <- sum(ms)
+pis <- ms / m_tot
+
+simld_power <- sapply(ns, function(n) pow_R_e(0.9, n = n, h2 = h2,
+                                           gammas = gammas, pis = pis))
+# problem with 0 pis
+
+# SCZ
+h2 <- 0.2633
+ms <- c(7276536.7375, 77483.0705, 2495.0030, 2.6315, 0.0575)
+m_tot <- sum(ms)
+pis <- ms / m_tot
+
+scz_power <- m_tot * sapply(ns, function(n) pow_R_e(0.9, n = n, h2 = h2,
+                                              gammas = gammas, pis = pis))
+plot(ht_power ~ ns, log = 'xy', type = "l", col = 2)
+
